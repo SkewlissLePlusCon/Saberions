deec75cdf6ca309a1328dfaa86197b83bbef69da
SkewlissLePlusCon <58728102+SkewlissLePlusCon@users.noreply.github.com>
2019-12-11 05:46:19 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,37 +27,4 @@ fs.readdir('./Events/', (error, f) => {
 });
 
 
-const DiscordAntiSpam = require("discord-anti-spam");
-const AntiSpam = new DiscordAntiSpam({
-  warnThreshold: 3, 
-  banThreshold: 7, 
-  maxInterval: 2000, 
-  warnMessage: "{@user}, S'il te plait arrête de spammer", 
-  banMessage: "**{user_tag}** a été banni pour avoir spam.", 
-  maxDuplicatesWarning: 7, 
-  maxDuplicatesBan: 15, 
-  deleteMessagesAfterBanForPastDays: 1, 
-  exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR", "MANAGE_GUILD", "BAN_MEMBERS"], 
-  ignoreBots: true, 
-  verbose: false, 
-  ignoredUsers: [], 
-  ignoredRoles: [], 
-  ignoredGuilds: [], 
-  ignoredChannels: [] 
-});
- 
-AntiSpam.on("warnEmit", (member) => console.log(`Attempt to warn ${member.user.tag}.`));
-AntiSpam.on("warnAdd", (member) => console.log(`${member.user.tag} a été averti.`));
-AntiSpam.on("kickEmit", (member) => console.log(`Attempt to kick ${member.user.tag}.`));
-AntiSpam.on("kickAdd", (member) => console.log(`${member.user.tag} a été kick.`));
-AntiSpam.on("banEmit", (member) => console.log(`Attempt to ban ${member.user.tag}.`));
-AntiSpam.on("banAdd", (member) => console.log(`${member.user.tag} a été banni.`));
-AntiSpam.on("dataReset", () => console.log("Le module cache a été clear."));
- 
-client.on("ready", () => console.log(`Logged in as ${client.user.tag}.`));
- 
-client.on("message", (msg) => {
-  AntiSpam.message(msg);
-});
-
 client.login(process.env.TOKEN);
